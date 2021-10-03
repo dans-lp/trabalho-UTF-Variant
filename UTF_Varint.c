ece4bd5e7a293310a97834206fe5ace4210c1c9d
Dans <danielperalta98@gmail.com>
2021-10-03 00:16:47 -0300

    implementação conversão de UTF8 para Hexa porém errada pois implementei pensando no sentido contratio de bytes - adição de função write_varint e sua descrição no header

--- UTF_Varint.c
+++ UTF_Varint.c
@@ -16,6 +16,9 @@ FILE *openFile(char *path, char *mode);
 //função exibe na tela conteúdo do arquivo
 void PrintContent_test(FILE * f_test);
 
+//função converte sequencia de bytes em Hex para Varint e escreve no arquivo
+void write_varint(unsigned char *ByteSeq);
+
 //---------------------------------------------->main<-------------------------------------------------
 
 int main(void){
@@ -23,6 +26,7 @@ int main(void){
 
    FILE *fin = openFile("/mnt/d/Trabalhos PUC/Software Basico/TrabalhoUTF-Variant/Unicode_pequeno.txt","rb");
    FILE *fout = openFile("/mnt/d/Trabalhos PUC/Software Basico/TrabalhoUTF-Variant/saida_teste.txt","wb");
+   //PrintContent_test(fin);
    res = utf_varint(fin,fout);
    fclose(fout);
    fclose(fin);
@@ -60,41 +64,57 @@ FILE *openFile(char *path, char *mode)
    return f;
 }
 
+
+
+
+
+
+
 int utf_varint(FILE *arq_entrada, FILE *arq_saida){
    
-   //PrintContent_test(arq_entrada);
-
    printf("\n------------------> inicio da função <------------------\n");
    unsigned char byteCurrent;
-   //unsigned char byteBefore = 0x00;
+   unsigned char byteSeq[4];
    //printf("   caracter %c <-> %02X \n", cUTF, cUTF );
-  
-   //loop leitura do arquivo
-   
+      
 
-   
+   //loop leitura do arquivo
    for (byteCurrent = getc(arq_entrada); !feof(arq_entrada); byteCurrent = getc(arq_entrada))
    {
-      printf("\nteste byte loop: %02X \n",byteCurrent);  
-      //printf("\nteste AND byte loop: %02X\n",(byteCurrent & 0x80));
-      int i = 0;
-      
-      while ((byteCurrent & 0x80) != 0x00)
+      printf("\nteste byte entrada loop: %02X \n",byteCurrent);  
+      int SeqNumber = 0;
+
+      if (byteCurrent >= 0x80)
       {
-         printf("\n >>>>>> MSByte diferente de zero -> nº de 1s: %d <<<<<<<<<\n",i);
-         byteCurrent = byteCurrent << 1;
-         i++;
-         //break;
+         printf(">>>>>> este byte possui continuação! <<<<<<<<<\n");
+         int i = 0;
+
+         while (byteCurrent >= 0x80)
+         {
+            byteCurrent <<= 1;
+            i++;
+         }
+         printf(">>>>>> nº de 1s: %d <<<<<<<<<\n\n",i);
+         byteCurrent >>= i;
+
+         byteSeq[SeqNumber] = byteCurrent;
+         SeqNumber++;
+         if (i > 1)
+         {
+            for (int k = 0; k < (SeqNumber - 1); k++)
+            {
+               byteSeq[k] &= (byteSeq[k+1] << 6);
+               
+               if (k>1) {byteSeq[k] >>= 2;}
+               fwrite(&byteSeq[k],sizeof(unsigned char),1,arq_saida);
+            }
+            fwrite(&byteSeq[3], sizeof(unsigned char),1,arq_saida);
+            SeqNumber = 0;
+         }
       }
-      
-      byteCurrent = byteCurrent >> i;
 
+      else {fwrite(&byteCurrent,sizeof(unsigned char), 1, arq_saida);}  
    }
-
-
-
-
    return 0;
 }
-
 
